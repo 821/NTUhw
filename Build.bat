0c1ae63b929744f85254ec4433f62a4a95b404b8
821 <foresterlian@gmail.com>
2020-11-11 20:53:04 +0800

    2020/11/11 20:53

--- Build.bat
+++ Build.bat
@@ -1,5 +1,5 @@
 @echo off
-xelatex.exe --synctex=-1 NTUHist
-biber --bblencoding=utf8 -l zh__stroke NTUHist
-xelatex.exe --synctex=-1 NTUHist
+xelatex.exe --synctex=-1 report
+biber --bblencoding=utf8 -l zh__stroke report
+xelatex.exe --synctex=-1 report
 :: del /q *.aux *.bbl *.blg *.log *.out *.toc *.bcf *.xml *.synctex *.nlo *.nls *.bak *.ind *.idx *.ilg *.lof *.lot *.ent-x *.tmp *.ltx *.los *.lol *.loc *.listing *.gz *.synctex(busy)
--- readme.md
+++ readme.md
@@ -1,3 +1,2 @@
-This is a XeLaTeX template for reports in Division of History, Taihoku Imperial University.
-Please read NTUHist.pdf before you start using this template.
+Please read Report.pdf before you start using this template.
 Basic knowledge of XeLaTeX and biblatex is also required.
